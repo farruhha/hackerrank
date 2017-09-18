8c4a43b10818d940cf03b8d6e151cdb83004dc23
farruhhabibullaev <germany-2013@mail.ru>
2017-09-18 18:02:17 -0500

    Arrays 2D Arrays Hello World

---
+++ src/datastructures/array/Arrays.java
@@ -0,0 +1,22 @@
+package datastructures.array;
+
+import java.util.Scanner;
+
+/**
+ * Created by farruhhabibullaev on 9/18/17.
+ */
+public class Arrays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        int[] intArray = new int[length];
+        for (int i = 0; i < length; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        for (int j = length - 1; j >= 0; j--) {
+            System.out.print(intArray[j]+" ");
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
