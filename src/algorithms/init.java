110dc6e73145e9b811d876618135b4e4fdc22f5d
farruhhabibullaev <germany-2013@mail.ru>
2017-09-18 11:10:55 -0500

    package update

---
+++ src/algorithms/init.java
@@ -0,0 +1,7 @@
+package algorithms;
+
+/**
+ * Created by farruhhabibullaev on 9/18/17.
+ */
+public class init {
+}
---
+++ src/datastructures/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by farruhhabibullaev on 9/18/17.
+ */
+package datastructures;
